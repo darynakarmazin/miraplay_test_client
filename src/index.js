7ed51dda62e8d5c344e6bbe801168ba031077b1a
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-11-15 12:08:21 +0200

    Added req for games list + basic styles.

--- src/index.js
+++ src/index.js
@@ -7,15 +7,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
 
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/miraplay_test_client">
+  // <React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="/miraplay_test_client">
+        <QueryClientProvider client={queryClient}>
           <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
+  // </React.StrictMode>
 );
--- src/pages/GameCategoriesPage/GameCategoriesPage.jsx
+++ src/pages/GameCategoriesPage/GameCategoriesPage.jsx
@@ -1,5 +1,65 @@
+import axios from "axios";
+import styles from "./GameCategoriesPage.module.css";
+import { useQuery } from "react-query";
+
+async function fetchGames() {
+  const { data } = await axios.post(
+    `https://api.miraplay.cloud/games/by_page`,
+    {
+      page: 1,
+      isFreshGamesFirst: true,
+      genre: false,
+      gamesToShow: 9,
+    }
+  );
+  return data.games;
+}
+
 function CategoriesPage() {
-  return <div>ВСІ ІГРИ</div>;
+  const { data, isLoading, isError } = useQuery("games", fetchGames);
+  console.log(data);
+  if (isLoading) return <p>Завантаження...</p>;
+  if (isError) return <p>Помилка завантаження даних</p>;
+  if (!data) return <p>Дані відсутні</p>;
+
+  return (
+    <div>
+      <h1 className={styles.categoriesPage_title}>ВСІ ІГРИ</h1>
+      <ul className={styles.categoriesPage_gameList}>
+        {data.map((game) => {
+          return (
+            <li key={game._id} className={styles.gameList_item}>
+              <img
+                className={styles.gameList_img}
+                src={game.gameImage}
+                alt="game view"
+                width={335}
+                height={335}
+              />
+              <div className={styles.gameList_trumb}>
+                <h3 className={styles.gameTrumb_title}>
+                  {game.commonGameName}
+                </h3>
+                <p className={styles.gameTrumb_desc}>{game.gameDescription}</p>
+              </div>
+              <div className={styles.gameList_genres}>
+                <div className={styles.gameList_genresTop}>
+                  {game.inTop && <p className={styles.gameList_top}>TOP</p>}
+                  <p className={styles.gameList_genre}>{game.genre}</p>
+                </div>
+                {game.gameClass === "STANDART" && (
+                  <p className={styles.gameList_free}>FREE</p>
+                )}
+              </div>
+              {/* <div>
+                <p>{game.gameLaunchers[0]}</p>
+              </div> */}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default CategoriesPage;
